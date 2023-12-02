6c2d50a5dd7bf9e153189c533d2955dafb99d91b
Edison Montes M <edison.montes@gmail.com>
2023-12-02 13:28:25 -0300

    Update raspi-python-installer.sh

--- raspi-python-installer.sh
+++ raspi-python-installer.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-echo ''
-echo '----------------------------------------------------'
-echo ''
-date
+echo 'Updating repositories'
+echo '---------------------'
 sudo apt-get update
+echo ''
+echo 'Installing all python dependencies'
+echo '----------------------------------'
 sudo apt-get install python3-full python3-pip
+echo ''
+echo 'Performing post installation cleanup'
+echo '------------------------------------'
 sudo apt-get autoclean -y
 sudo apt-get autoremove -y
